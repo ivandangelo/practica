3a5b861d0d0bd2d379e1c08a6c79ebf5559bcc48
ivandangelo <ivanuntref@gmail.com>
2020-08-25 17:21:39 -0300

    ej 9

---
+++ src/9).c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void PosONeg(){
+	double i;
+	
+	printf("ingrese un numero: ");
+	scanf("%d",i);
+	
+	if((-1)*i == i){
+		
+		(i==0) ? (printf("%d es cero\n",i)) : (printf("%d es positivo\n",i));
+		
+		
+		
+	}
+	
+	
+	
+}
